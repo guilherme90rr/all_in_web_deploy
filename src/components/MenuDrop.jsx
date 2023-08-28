f03df2459939160b30f38b9f3b370f34f6a75698
rm95542 <rm95542@fiap.com.br>
2023-08-28 18:38:42 -0300

    Atividade_28_08

---
+++ src/components/MenuDrop.jsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from 'react';
+
+export default function MenuDrop() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+
+
+        <ChevronDown className='text-slate-300' />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Deletar</MenuItem>
+        <MenuItem onClick={handleClose}>Modificar</MenuItem>
+      </Menu>
+    </div>
+  );
+}
