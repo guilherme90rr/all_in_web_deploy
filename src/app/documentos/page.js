8f165a6c4b0d0523ff270358e7371cf90520309f
rm95542 <rm95542@fiap.com.br>
2023-09-25 19:24:17 -0300

    Update e delete.

--- src/app/documentos/page.js
+++ src/app/documentos/page.js
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 
 
 async function getDocumentos(){
-  const url = "http://localhost:8080/api/contas"
+  const url = "http://localhost:8080/api/pages"
   const resp = await fetch(url, { next: { revalidate: 0 } })
   if (!resp.ok) throw new Error("Não pode carregar os dados")
   return resp.json()
@@ -19,10 +19,37 @@ export default async function Home() {
         <h2 className="text-2xl font-bold">Documentos</h2>
 
         <div id="data" className="text-slate-300 m-1">
-          {data.map(documentos => <DataRow conta={documentos} /> )}
+          {data.map(documentos => <DataRow page={documentos} /> )}
         </div>
       </main>
     </>
 
   )
 }
+
+export async function destroy(id){  
+  const deleteUrl = url + "/" + id
+  const options = {
+      method: "DELETE"
+  }
+  const resp = await fetch(deleteUrl, options)
+  if (resp.status !== 204) return {error: "Erro ao apagar seu documento. " + resp.status}
+  revalidatePath("/page")
+
+}
+
+
+export async function update(page){
+  const updateURL = url + "/" + page.id
+  const options = {
+      method: "PUT",
+      body: JSON.stringify(page),
+      headers: {
+          "Content-Type": "application/json"
+      }
+  }
+  const resp = await fetch(updateURL, options)
+  if (resp.status !== 200) return {error: "Erro ao atualizar seu documento. " + resp.status}
+  revalidatePath("/")
+}
+
--- src/components/MenuDrop.jsx
+++ src/components/MenuDrop.jsx
@@ -12,6 +12,15 @@ export default function MenuDrop() {
     setAnchorEl(null);
   };
 
+  const handleDelete = async () => {
+    const resp = await destroy(page.id)
+    if (resp?.error) {
+      toast.error(resp.error)
+      return
+    }
+    toast.success("documento apagada com sucesso")
+  }
+
   return (
     <div>
       <Button
@@ -36,6 +45,7 @@ export default function MenuDrop() {
       >
         <MenuItem onClick={handleClose}>Deletar</MenuItem>
         <MenuItem onClick={handleClose}>Modificar</MenuItem>
+        <AlertDialogAction onClick={handleDelete}>sim, pode apagar esse documento</AlertDialogAction>
       </Menu>
     </div>
   );
